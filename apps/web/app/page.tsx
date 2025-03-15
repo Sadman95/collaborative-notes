77b0153dcda0db7ba292e94ba6233cf54fef9a0d
Sadman Sakib <sakibsadman108@gmail.com>
2025-03-15 20:29:09 +0600

    setup:initial commit

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen p-6">
+   
+        <h1 className="text-4xl font-bold">Collaborative Notes</h1>
+       
+    </main>
+  );
+}
